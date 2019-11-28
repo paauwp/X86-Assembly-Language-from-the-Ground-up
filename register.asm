22eab21a859154ba1c94e5d32af50fadc2ef8247
Pieter Paauw <39484121+paauwp@users.noreply.github.com>
2019-11-28 16:41:57 +0100

    Add files via upload

---
+++ register.asm
@@ -0,0 +1,21 @@
+;.code
+;main	proc
+		;mov rax,15
+		;mov rbx,3
+		;mov rcx,4
+		;ret
+
+;main	endp
+;end
+
+.386
+.model flat 
+.code
+main	proc
+		mov eax,15
+		mov ebx,3
+		mov ecx,4
+		ret
+
+main	endp
+end		main
