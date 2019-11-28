25f8de9221db213cf683e1e752c68eec618ba1fc
Pieter Paauw <39484121+paauwp@users.noreply.github.com>
2019-11-28 17:10:50 +0100

    Add files via upload

--- register.asm
+++ register.asm
@@ -1,3 +1,4 @@
+;...................x86 64bit.......................
 ;.code
 ;main	proc
 		;mov rax,15
@@ -8,13 +9,29 @@
 ;main	endp
 ;end
 
+;...................x86 32bit.......................
+;.386
+;.model flat 
+;.code
+;main	proc
+		;mov eax,15
+		;mov ebx,3
+		;mov ecx,4
+		;ret
+
+;main	endp
+;end		main
+
+
+;...................16 bit..........................
 .386
 .model flat 
 .code
 main	proc
-		mov eax,15
-		mov ebx,3
-		mov ecx,4
+
+		mov ax,15
+		mov bx,3
+		mov cx,4
 		ret
 
 main	endp
